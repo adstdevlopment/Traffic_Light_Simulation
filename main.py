49c845faf83f760901ed2427f50c019b643fb4ee
adstdevlopment <adrien.dos-santos@outlook.fr>
2022-07-17 22:30:05 +0200

    Add of the ui

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 # Traffic Light Simulation
+from PyQt5 import QtWidgets
 from GUI.Interface import Interface
+from GUI.gui_mainwindow import Ui_MainWindow
 
 
 def main():
     interface = Interface()
     interface.start()
+
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QWidget()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
